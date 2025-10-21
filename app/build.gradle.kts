87eb6a1b4a3ca1af17fd9457f0ab6497edf83f36
nazmulcse247 <nazmulhoque247@gmail.comn>
2025-10-21 17:43:12 +0600

    BottomNavExtension file changed

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,12 +10,12 @@ plugins {
 
 android {
     namespace = "com.iamnazmul.tmdbmovie"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.iamnazmul.tmdbmovie"
         minSdk = 24
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
